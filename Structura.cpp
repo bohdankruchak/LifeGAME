dd845fea33f9fbecf660f73c0000dc585938c087
bohdankruchak <kruchak_bd@fizmat.tnpu.edu.ua>
2015-11-22 18:19:38 +0200

    1

---
+++ Structura.cpp
@@ -0,0 +1,25 @@
+кнопка Fast start
+g_Workspace.hFieldMutex = CreateMutex(NULL, TRUE, NULL);
+g_Workspace.hRunMutex = CreateMutex(NULL, TRUE, NULL);
+if (!SetTimer(IDT_TIMER1, 1000, NULL)){
+   ReleaseMutex(g_Workspace.hRunMutex);		
+   RedrawWindow();
+   _beginthread(this->FastStartProc, 4096, NULL);
+   if (g_Workspace.stop == 1){ KillTimer(IDT_TIMER1); }
+  }  
+CloseHandle(g_Workspace.hFieldMutex);
+CloseHandle(g_Workspace.hRunMutex);
+
+структура процедури FastStart
+        do
+	{
+		WaitForSingleObject(g_Workspace.hFieldMutex, INFINITE);
+		\\\\\\\\\\\\\
+		виконую дії алгоритму гри
+		\\\\\\\\\\\\\
+		ReleaseMutex(g_Workspace.hFieldMutex);	
+		присвоюю загальному полю значення обрахованого
+		g_Workspace.last = g_Workspace.that;
+		g_Workspace.that = g_Workspace.new_t;
+	}
+	while (WaitForSingleObject(g_Workspace.hRunMutex, 75L) == WAIT_TIMEOUT);
